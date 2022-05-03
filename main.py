ba689d28e5f2b7abb7b66235b0b2e119752505d7
unikhex <potatoc377@gmail.com>
2022-05-03 12:59:58 +0200

    created an input in which a person can just copy a link of their choice and the qr code will do its stuff

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pyqrcode
-from pyzbar.pyzbar import  decode
-from PIL import Image
+import pyqrcode #was easiliy installed using pip now just import
+from pyzbar.pyzbar import  decode # the decoder used to decode the qr code above
+from PIL import Image #changes the qr code that i have to an image that is saved else where
 
-qr = pyqrcode.create("First try lets see if it worked")
+qr = pyqrcode.create(str(input("Write here your link : ")))
 qr.png("letsgo.png", scale = 8)
 
-d = decode (image.open("letsgo.png"))
+d = decode (Image.open("letsgo.png"))# had a problem with the image
+# didnt know that without capital letter I it wont work had to look for a couple of minutes what the fuck was wrong with this code
 print(d[0].data.decode("ascii"))
